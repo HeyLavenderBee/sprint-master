6e8b70b17b267122ef8a268dc9b666ce4cb31ed1
Júlia Rodrigues <heylavenderbee@gmail.com>
2026-05-20 10:12:37 -0300

    feat(questionario): faz integração imagens backend
    As imagens do backend fazem alteração aqui

--- projeto/public/assets/js/questionario.js
+++ projeto/public/assets/js/questionario.js
@@ -6,6 +6,7 @@ const alternativeA = document.getElementById("text-a");
 const alternativeB = document.getElementById("text-b");
 const alternativeC = document.getElementById("text-c");
 const alternativeD = document.getElementById("text-d");
+const questionImage = document.getElementById("question-image");
 
 function setQuestionNumberIndicator(idQuestao, numero) {
   questionNumberIndicator.innerHTML = `Questão ${idQuestao}`;
@@ -13,12 +14,17 @@ function setQuestionNumberIndicator(idQuestao, numero) {
   progressIndicator.style.width = `${percentage}%`;
 }
 
-function setQuestionHtml(question, a, b, c, d) {
+function setQuestionHtml(question, a, b, c, d, image) {
   questionTextElement.textContent = question;
   alternativeA.textContent = a;
   alternativeB.textContent = b;
   alternativeC.textContent = c;
   alternativeD.textContent = d;
+  if(image != null){
+    questionImage.src = `assets/img/questionario/${image}`;
+  } else{
+    questionImage.src = "";
+  }
 }
 
 async function getQuestion() {
@@ -27,7 +33,8 @@ async function getQuestion() {
 
   const response = await fetch(endpoint, {
     method: "GET",
-    headers: { Authorization: `Bearer ${token}` },
+    //headers: { Authorization: `Bearer ${token}` },
+    headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c3VhcmlvIjozMywiaWF0IjoxNzc5MjgyNDQyLCJleHAiOjE3NzkyODM2NDJ9.vDBCsIiqtZ3yzirQ3R4rhRO-DtkMZwGMzRsJEoeKa9M` },
   });
 
   const data = await response.json();
@@ -36,7 +43,7 @@ async function getQuestion() {
     window.location.href = "resultado-questionario.html";
   }
   setQuestionNumberIndicator(data.id_questao, data.numero);
-  setQuestionHtml(data.enunciado, data.alternativa_a, data.alternativa_b, data.alternativa_c, data.alternativa_d);
+  setQuestionHtml(data.enunciado, data.alternativa_a, data.alternativa_b, data.alternativa_c, data.alternativa_d, data.imagem);
 }
 
 async function nextQuestion() {
@@ -47,7 +54,8 @@ async function nextQuestion() {
     method: "GET",
     headers: {
       Authorization:
-        `Bearer ${token}`,
+        //`Bearer ${token}`,
+        `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c3VhcmlvIjozMywiaWF0IjoxNzc5MjgyNDQyLCJleHAiOjE3NzkyODM2NDJ9.vDBCsIiqtZ3yzirQ3R4rhRO-DtkMZwGMzRsJEoeKa9M`,
     },
   });
   const data = await response.json();
@@ -67,7 +75,8 @@ async function nextQuestion() {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
+      //Authorization: `Bearer ${token}`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c3VhcmlvIjozMywiaWF0IjoxNzc5MjgyNDQyLCJleHAiOjE3NzkyODM2NDJ9.vDBCsIiqtZ3yzirQ3R4rhRO-DtkMZwGMzRsJEoeKa9M`,
     },
     body: JSON.stringify({ id_exame, id_questao, resposta }),
   });
