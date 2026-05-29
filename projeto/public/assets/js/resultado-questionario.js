747971f35e3dddc3e13f3cd251c2736570cadcb5
Mari432hz <maritav200811@gmail.com>
2026-05-28 22:44:20 -0300

    feat(frase-resultado): inicia task
    Cria a função com os arrays e um if incompleto.

---
+++ projeto/public/assets/js/resultado-questionario.js
@@ -0,0 +1,13 @@
+const fraseConsolo = document.getElementById("frase-consolo");
+// const fraseConsolo = document.getElementById("frase-consolo");
+// const fraseConsolo = document.getElementById("frase-consolo");
+// const fraseConsolo = document.getElementById("frase-consolo");
+
+function fraseConsolo(nota){
+    let fraseSucessso = ["Parabéns, você alcançou o resultado necessário.", "Muito bem, você entendeu!", "Ótimo trabalho!"];
+    let fraseFracasso = ["Ops, não foi dessa vez...", "Iih, parece que você não passou...", "Poxa, fica para depois né?"];
+
+    if(nota >= 70){
+        fraseConsolo.innerHTML = `` 
+    }
+}
