23d9434680e8dac02cfb64e4d388889b9f78640f
Mari432hz <maritav200811@gmail.com>
2026-06-01 22:45:54 -0300

    feat(frase-resultado): finaliza task
    Cria a função ShowMessage que deve exibir mensagens para o usuário conforme sua nota, no entanto não pude testar adequadamente.

--- projeto/public/assets/js/resultado-questionario.js
+++ projeto/public/assets/js/resultado-questionario.js
@@ -1,13 +1,35 @@
 const fraseConsolo = document.getElementById("frase-consolo");
-// const fraseConsolo = document.getElementById("frase-consolo");
-// const fraseConsolo = document.getElementById("frase-consolo");
-// const fraseConsolo = document.getElementById("frase-consolo");
 
-function fraseConsolo(nota){
-    let fraseSucessso = ["Parabéns, você alcançou o resultado necessário.", "Muito bem, você entendeu!", "Ótimo trabalho!"];
-    let fraseFracasso = ["Ops, não foi dessa vez...", "Iih, parece que você não passou...", "Poxa, fica para depois né?"];
+function showMessage(nota) {
+  const fraseSucessso = [
+    "Parabéns, você alcançou o resultado necessário.",
+    "Muito bem, você entendeu!",
+    "Ótimo trabalho!",
+  ];
+  const fraseFracasso = [
+    "Ops, não foi dessa vez...",
+    "Iih, parece que você não passou...",
+    "Poxa, fica para depois né?",
+  ];
 
-    if(nota >= 70){
-        fraseConsolo.innerHTML = `` 
-    }
+  // Calcula um índice aleatório baseado no tamanho do array
+  if (typeof nota !== "number" || isNaN(nota)) {
+    alert("A nota é inválida");
+    return;
+  } else if (nota >= 7) {
+    const indiceAleatorio = Math.floor(Math.random() * fraseSucessso.length);
+    const elementoAleatorio = fraseSucessso[indiceAleatorio];
+    fraseConsolo.innerText = elementoAleatorio;
+
+    console.log(elementoAleatorio);
+  } else {
+    const indiceAleatorio = Math.floor(Math.random() * fraseFracasso.length);
+    const elementoAleatorio = fraseFracasso[indiceAleatorio];
+    fraseConsolo.innerText = elementoAleatorio;
+
+    console.log(elementoAleatorio);
+  }
+
 }
+
+showMessage(nota);
