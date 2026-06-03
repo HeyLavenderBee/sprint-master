ac8f489af695701ced77c0163c4e269e1af7798c
gm-paiva <g.matos.p96@gmail.com>
2026-06-02 22:30:42 -0300

    feat(adicao-render): inicia a compatibilidade com render
    .env preparado e db.js editado para o lançamento do projeto no render.

--- projeto/src/database/db.js
+++ projeto/src/database/db.js
@@ -8,12 +8,27 @@ dotenv.config({
 
 const { Pool } = require("pg");
 
-const config = {
+// const config = {
+//   host: process.env.POSTGRES_HOST,
+//   user: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DB,
+//   port: process.env.POSTGRES_PORT,
+// };
+
+let config;
+if (process.env.DATABASE_URL) {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+  };
+} else {
+  config = {  
   host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   port: process.env.POSTGRES_PORT,
+}
 };
 
 const pool = new Pool(config);
