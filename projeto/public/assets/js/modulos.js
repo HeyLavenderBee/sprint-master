2ef1003578ad112ea7675f5266f2b7bb52638cc4
gm-paiva <g.matos.p96@gmail.com>
2026-06-08 23:04:09 -0300

    feat(integ-modulos): finaliza integração back e front modulos
    foi realizado a integração e adaptação do front e back de módulos

--- projeto/public/assets/js/modulos.js
+++ projeto/public/assets/js/modulos.js
@@ -1,5 +1,4 @@
 const divAlerta = document.getElementById("alerta-tentativas");
-
 function alterarAviso() {
   divAlerta.innerHTML = `<h3 class="texto-aviso">
           Parabéns, você concluiu todos os módulos 🎉
@@ -8,7 +7,7 @@ function alterarAviso() {
   showConfetti();
 }
 
-function showConfetti(){
+function showConfetti() {
   confetti({
     position: { x: window.innerWidth / 2, y: window.innerHeight / 2 },
     count: 200,
@@ -55,3 +54,62 @@ async function verificarModulos() {
 }
 
 verificarModulos();
+
+async function getModulos() {
+  const token = localStorage.getItem("token");
+  const endpoint = `api/questoes/modulos-respondidos`;
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (!response.ok) {
+    return;
+  }
+
+  const data = await response.json();
+
+  var currentModule = 1;
+  for (let i = 0; i < data.length; i++) {
+    if (i == data.length - 1) {
+      currentModule = data[i].id_modulo;
+    }
+  }
+  return currentModule + 1;
+  
+}
+
+getModulos();
+
+
+async function setModulos() {
+  const currentModule = await getModulos();
+  for (let i = 1; i <= currentModule; i++) {
+    const moduloLiberado = document.getElementById(
+      `btn-modulo-${i}`,
+    );
+    moduloLiberado.classList.remove("blocked");
+    moduloLiberado.innerText = "Acessar";
+    moduloLiberado.setAttribute("href", "questionario.html");
+  }
+}
+
+setModulos();
+
+async function setCompletedModules() {
+  const currentModule = await getModulos();
+  if (currentModule > 1) {
+    for (let i = 1; i < currentModule; i++) {
+      const moduloConcluido = document.getElementById(
+        `btn-modulo-${i}`,
+      );
+      moduloConcluido.classList.add("done");
+      moduloConcluido.innerText = "Feito!";
+    moduloConcluido.removeAttribute("href");
+
+    }
+  }
+}
+
+setCompletedModules();
+
